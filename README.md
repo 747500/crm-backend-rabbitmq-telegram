aab368306cac2c6660569d708452c4f46c1adaba
Serge R <747500@gmail.com>
2021-03-17 19:41:01 +0300

    updates for rabbitmq

---
+++ README.md
@@ -0,0 +1,14 @@
+# crm-tg-bot
+
+Telegram bot for https://github.com/747500/crm
+
+Prereq:
+
+- Telegram bot token
+- config.js (from example.config.js)
+- RabbitMQ
+
+
+$ npm install
+
+$ npm run serve
--- example.config.mjs
+++ example.config.mjs
@@ -1,8 +1,31 @@
 
 export default {
 
-	Token: '',
+	telegram: {
+		Token: '',
 
-	loggerDir: './msglog',
+		// for logging all messages to
+		loggerDir: './msglog',
+	},
+
+	amqp: {
+
+		url: 'amqp://127.0.0.1',
+
+		// Queue for incoming messages to forward to telegram user.
+		// asserted here
+		botQueue: {
+			name: 'crm_untyped_net_bot',
+			properties: {
+				exclusive: true,
+				durable: true,
+			}
+		},
+
+		// Queue for outgoing requests.
+		// asserted by crm
+		authQueue: 'crm_bot_auth',
+
+	},
 
 }
